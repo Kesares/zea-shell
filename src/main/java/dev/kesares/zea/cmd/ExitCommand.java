927ece7a3a83400f0e728dff4449404309f63b13
Kesares <kesares19@gmail.com>
2026-05-31 14:03:17 +0200

    Add exit status argument support

--- src/main/java/dev/kesares/zea/cmd/ExitCommand.java
+++ src/main/java/dev/kesares/zea/cmd/ExitCommand.java
@@ -17,6 +17,19 @@ public class ExitCommand implements Command {
             ShellIO.errln(this.getName() + ": too many arguments");
             return ExitCode.USAGE_ERROR.code();
         }
+
+        if (args.length == 1) {
+            try {
+                int code = Integer.parseInt(args[0]);
+                context.stop();
+                return code;
+            } catch (NumberFormatException e) {
+                ShellIO.errln(this.getName() + ": numeric argument required");
+                context.stop();
+                return ExitCode.USAGE_ERROR.code();
+            }
+        }
+
         context.stop();
         return ExitCode.SUCCESS.code();
     }
